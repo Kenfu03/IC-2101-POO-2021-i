11fbe0b9bac6ba5663d5aadb2c9aeafd328eefaa
Kenneth <kennethesa324@gmail.com>
2021-05-21 16:28:41 -0600

    Configuración de la computadora y ejecución de código de muestra

---
+++ Lab-5/src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
+public class Controller {
+    private float min = -171.0f;
+    private float max = 171.0f;
+
+
+    @FXML
+    private ImageView dardo;
+    public void lanzarDado(ActionEvent event){
+        double randomX = min + Math.random() * (max - min);
+        double randomY = min + Math.random() * (max - min);
+        dardo.setX(randomX);
+        dardo.setY(randomY);
+    }
+}
